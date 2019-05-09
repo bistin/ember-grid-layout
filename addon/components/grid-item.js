7a627f167a394087362bc4739ba91f5a8e7fe87b
bistin <bistin@gmail.com>
2019-05-09 23:42:56 +0800

    Make It Work On firefox

--- addon/components/grid-item.js
+++ addon/components/grid-item.js
@@ -1,9 +1,6 @@
 import Component from '@ember/component';
 import { computed } from '@ember/object';
 import { htmlSafe } from '@ember/string';
-//import layout from '../templates/components/grid-item';
-import { compact } from "ember-grid/utils";
-import { setProperties } from "@ember/object";
 import { throttle } from '@ember/runloop';
 
 export default Component.extend({
@@ -17,7 +14,7 @@ export default Component.extend({
 
     styleText: computed('pos.{x,y,w,h}', function() {
         if(!this.pos){ return "";}
-        const {x, y, w, h} = this.pos;
+        const { x, y, w, h } = this.pos;
         const p = this.calcPosition(x, y, w, h)
         return htmlSafe(`height:${p.height}px;width:${p.width}px;left:${p.left}px;top:${p.top}px`);
     }),
@@ -44,19 +41,7 @@ export default Component.extend({
 
         return out;
     },
-    calcXY(top, left) {
-        const { margin, cols, rowHeight, maxRows } = this.grid;
-        const { w, h } = this.pos;
-        const colWidth = this.calcColWidth();
 
-        let x = Math.round((left - margin[0]) / (colWidth + margin[0]));
-        let y = Math.round((top - margin[1]) / (rowHeight + margin[1]));
-
-        // Capping
-        x = Math.max(Math.min(x, cols - w), 0);
-        y = Math.max(Math.min(y, maxRows - h), 0);
-        return { x, y };
-    },
 
     calcColWidth() {
         const { margin, containerPadding, containerWidth, cols } = this.grid;
@@ -65,54 +50,18 @@ export default Component.extend({
         );
     },
 
-    _dragMove (e) {
-        if(e.pageX === 0 && e.pageY === 0) {
-            return;
-        }
-        const newPosition = { top: 0, left: 0 };
-        const {x, y} = this.startPoint;
-        const deltaX = e.pageX - x;
-        const deltaY = e.pageY - y;
-
-        this.set('startPoint', {
-            x: e.pageX,
-            y: e.pageY
-        });
-
-        newPosition.left = this.dragging.left + deltaX;
-        newPosition.top = this.dragging.top + deltaY;
-
-        this.set("dragging", newPosition);
-        const pos = this.calcXY(newPosition.top, newPosition.left);
-        this.grid.onDrag(pos.x, pos.y, this.pos, this.index);
-        //yield timeout(80);
-    },
-
     actions: {
         remove() {
-            this.layoutModel.removeObject(this.pos);
-
-            const tmpArr = [...this.layoutModel].map(d => ({ ...d }));
-            let layout2 = compact(
-                tmpArr,
-                'vertical',
-                12 );
-
-            this.layoutModel.forEach((d, i) => {
-                setProperties(d, layout2[i]);
-            });
+            this.grid.remove(this.pos);
         },
 
-        dragMoveAction(e) {
-            // add throttle
-            this.dragMove(e);
-        },
         dragStartAction(e) {
             const newPosition = { top: 0, left: 0 };
             this.set('startPoint', {
                 x: e.pageX,
                 y: e.pageY
             });
+
             let node = e.target
             const { offsetParent } = node;
             node = node.children[0];
@@ -123,21 +72,12 @@ export default Component.extend({
                 clientRect.left - parentRect.left + offsetParent.scrollLeft;
             newPosition.top =
                 clientRect.top - parentRect.top + offsetParent.scrollTop;
-            //debugger
+
             this.set("dragging", newPosition);
-            this.grid.onDragStart();
+            this.grid.onDragStart(newPosition, e.clientX, e.clientY, this.index);
         },
 
         dragEndAction() {
-            // const newPosition = { top: 0, left: 0 };
-            // newPosition.left = this.dragging.left;
-            // newPosition.top = this.dragging.top;
-            // this.set("dragging", null);
-            // const pos = this.calcXY(newPosition.top, newPosition.left);
-            // //if(pos.x !== 0 || pos.y !== 0){
-            // this.grid.onDrag(pos.x, pos.y, this.pos, this.index)
-            // //}
-            // this.set("dragging", null);
             this.grid.onDragStop();
         }
     }
