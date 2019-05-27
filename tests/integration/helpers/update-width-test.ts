7c1b522f2be15695a080f5a98c38ad21bbfba5be
bistin <bistin@gmail.com>
2019-05-28 05:04:02 +0800

    fix type error

--- tests/integration/helpers/update-width-test.ts
+++ tests/integration/helpers/update-width-test.ts
@@ -4,14 +4,17 @@ import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
 module('Integration | Helper | update-width', function(hooks) {
-  setupRenderingTest(hooks);
+    setupRenderingTest(hooks);
 
-  // Replace this with your real tests.
-  test('it renders', async function(assert) {
-    this.set('inputValue', '1234');
+    // Replace this with your real tests.
+    test('it renders', async function(assert) {
+        this.set('inputValue', '1234');
 
-    await render(hbs`{{update-width inputValue}}`);
+        await render(hbs`{{update-width inputValue}}`);
 
-    assert.equal(this.element.textContent.trim(), '1234');
-  });
+        const text = this.element.textContent;
+        if(text) {
+            assert.equal(text.trim(), '1234');
+        }
+    });
 });
