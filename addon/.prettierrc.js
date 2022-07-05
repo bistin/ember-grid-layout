b87f72f05b90ebda0102e7c952a8bbde8b0ad268
bistin <bistin@fugle.tw>
2022-07-06 00:09:48 +0800

    v2 addon

---
+++ addon/.prettierrc.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = {
+    singleQuote: true,
+    arrowParens: 'always',
+    bracketSpacing: true,
+    printWidth: 100,
+    proseWrap: 'preserve',
+    semi: true,
+    tabWidth: 4,
+    trailingComma: 'all',
+    useTabs: false,
+};
--- app/components/grid-layout.js
+++
@@ -1,1 +0,0 @@
-export { default } from 'ember-grid-layout/components/grid-layout';
